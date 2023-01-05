aa397ca2e6f771ef4d739e849ce164085b521800
Bipin <87369440+bipinkrish@users.noreply.github.com>
2023-01-05 22:36:01 +0530

    fix

--- annas.py
+++ annas.py
@@ -7,8 +7,26 @@ from os import remove
 
 
 def getAnnasBooks(searchbook):
+    headers = {
+    'authority': 'annas-archive.org',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'max-age=0',
+    'dnt': '1',
+    'if-modified-since': 'Sun, 01 Jan 2023 12:57:10 GMT',
+    'referer': 'https://annas-archive.org/',
+    'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    }
     params = {'q': searchbook}
-    response = get('https://annas-archive.org/search', params=params)
+    response = get('https://annas-archive.org/search', params=params, headers=headers)
     soups = BeautifulSoup(response.content,"html.parser")
     soups = soups.findAll("div",class_="mb-4")[-1].findAll("div",class_="h-[125]")
     books = []
